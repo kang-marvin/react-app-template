0312d3a34cfee09259a6a69a475e468e7f2affc2
Marvin Kang'ethe <marvin.kangethe@andela.com>
2019-07-24 16:12:12 +0300

    Document how to retrieve authorization token from Redux state and pass its value to Request header key `Authorization`.
     - Also add the DELETE request.

--- src/api/common/httpMethods.js
+++ src/api/common/httpMethods.js
@@ -1,18 +1,26 @@
 import URL from "./utils";
-import configureStore from "../../redux/store/configureStore";
 
 /** Retrieve authentication from saved state */
 
-const authentication = configureStore().getState().authentication;
+/**
+ * You can extract the authorization token from Redux store
+ *  (imported from configureStore) and
+ * pass it as Authorization parameter for the Request header.
+ *
+ * Example :
+ *  import configureStore from "../../redux/store/configureStore";
+ *  const authorization = configureStore().getState().authorization.token;
+ */
+
 const headers = new Headers(
   {
-    // "Authorization": authentication.access_token,
     "Content-Type": "application/json",
-    Accept: "application/json",
+    "Accept": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type"
   });
 
+/** GET requests */
 export async function get(url) {
   const response = await fetch(URL(url), {
     method: "GET",
@@ -25,9 +33,23 @@ export async function get(url) {
   };
 }
 
+/** POST requests */
 export async function post(url, body) {
   const response = await fetch(URL(url), {
     method: "POST",
+    headers: headers,
+    body: JSON.stringify(body)
+  });
+  return {
+    data: await response.json(),
+    status: response.status
+  };
+}
+
+/** DELETE requests */
+export async function remove(url, body) {
+  const response = await fetch(URL(url), {
+    method: "DELETE",
     headers: headers,
     body: JSON.stringify(body)
   });
