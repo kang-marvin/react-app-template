4e901b57c1ca4fb641c5d9f485c9dca1cef988f7
Marvin Kang'ethe <marvin.kangethe@andela.com>
2019-07-24 16:35:58 +0300

    Add the Redux DevTools package and attach it to all environments except Production.
     - I think the package needs to be under devDependencies

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { logger } from "redux-logger";
 import rootReducer from "../reducers";
 
 export default function configureState(initialState) {
   const middleWares =
     process.env.NODE_ENV !== "production"
-      ? [logger, reduxImmutableStateInvariant(), thunk]
+      ? [logger, reduxImmutableStateInvariant(), composeWithDevTools, thunk]
       : [thunk]
 
 
